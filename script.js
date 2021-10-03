27032c2172f4309a88a69f6aab568e6aefb3f290
Cedrick Garcia <icedgarcia@gmail.com>
2021-10-03 15:55:28 +0800

    cool ui idea :v:

---
+++ script.js
@@ -0,0 +1,25 @@
+// // const pos = document.querySelector('.hero')
+// // const blue =document.querySelector('.blue')
+// // pos.addEventListener('mousemove', e => {
+// //   pos.style.setProperty('--x', e.clientX + 'px')
+// //   pos.style.setProperty('--y', e.clientY + 'px')
+
+// //   console.log(pos)
+// // })
+
+// const lines = document.querySelectorAll('.lines')
+
+// function changeLineColor(e) {
+//   x = e.clientX
+//   y = e.clientY
+//   // lines.style.transform = `translate3d(${x}px, ${y}px, 0)`
+//   lines.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+
+//   lines.style.setProperty('--x', (x) + 'px')
+//   lines.style.setProperty('--y', (y) + 'px')
+//   lines.style.background = `#000000`
+//   console.log( x ,y)
+
+// }
+
+// lines.addEventListener('mousemove', changeLineColor )
